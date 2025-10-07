62b31ac08a8499f0c7c509d091d6ed51f144dce2
Paulo Henrique <phrocha8530@gmail.com>
2025-10-07 10:17:23 -0300

    Initial commit

---
+++ 1002_Area_do_Circulo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+#define pi 3.14159
+
+    int main(){
+
+        long double area, raio;
+
+        scanf("%Lf", &raio);
+        
+        area = pi * pow(raio, 2);
+
+        printf("A=%.4Lf\n", area);
+
+    }
